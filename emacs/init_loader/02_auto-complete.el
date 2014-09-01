b90278cb8feb1aa795c2abe2c26da6c8d4f40291
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2014-09-01 06:53:25 +0000

    Set up ruby-mode and auto-complete

---
+++ emacs/init_loader/02_auto-complete.el
@@ -0,0 +1,4 @@
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/.cask/24.3.1/elpa/auto-complete-20140824.1658/dict")
+(ac-config-default)
+(setq ac-use-menu-map t)
--- emacs/init_loader/03_ruby.el
+++ emacs/init_loader/03_ruby.el
@@ -1,12 +1,30 @@
 ;; --------------------------------------------------
 ;; ruby-mode
 ;; http://shibayu36.hatenablog.com/entry/2013/03/18/192651
+;; http://konboi.hatenablog.com/entry/2012/08/15/154315
 ;; --------------------------------------------------
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
+(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode)) ;; shebangがrubyの場合、ruby-modeを開く
+
+;; ruby-modeのインデントを改良する
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
 
 ;; --------------------------------------------------
 ;; ruby-end
@@ -28,3 +46,12 @@
 (require 'ruby-block)
 (ruby-block-mode t)
 (setq ruby-block-highlight-toggle t)
+
+;; --------------------------------------------------
+;; robe
+;; http://codeout.hatenablog.com/entry/2014/02/04/210237
+;; --------------------------------------------------
+(add-hook 'ruby-mode-hook 'robe-mode)
+(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
+(autoload 'ac-robe-setup "ac-robe" "auto-complete robe" nil nil)
+(add-hook 'robe-mode-hook 'ac-robe-setup)
