ef11a4a7089cd66672f5a2d995a63075d609602d
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2014-09-09 04:39:18 +0000

    Adding php-mode and web-mode

---
+++ emacs/init_loader/03_web-mode.el
@@ -0,0 +1,7 @@
+;; --------------------------------------------------
+;; web-mode
+;; http://rubikitch.com/2014/08/24/web-mode/
+;; --------------------------------------------------
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.smarty\\'" . web-mode))
