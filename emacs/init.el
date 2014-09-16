4664a2014d9014ba92dadd72b2293b1b9ec390aa
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2014-09-16 07:12:53 +0000

    Deleting emacs config

--- emacs/init.el
+++
@@ -1,8 +0,0 @@
-;; Emacs package system
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-
-;; init-loader
-(custom-set-variables
- '(init-loader-show-log-after-init 'error-only))
-(init-loader-load (concat user-emacs-directory "init_loader"))
--- emacs/init_loader/03_web-mode.el
+++
@@ -1,8 +0,0 @@
-;; --------------------------------------------------
-;; web-mode
-;; http://rubikitch.com/2014/08/24/web-mode/
-;; --------------------------------------------------
-(require 'web-mode)
-
-(add-to-list 'auto-mode-alist '("\\.smarty\\'" . web-mode));; Smarty
-(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))   ;; Handlebars.js
